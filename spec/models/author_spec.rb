7a25854f8f5cd72f32b006b2cf2fe7db78b4f3c8
Veniel Dorestave <veniel@noukod.com>
2019-01-25 14:49:21 +0000

    Automatically backed up by Learn

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,45 +1,21 @@
 require 'rails_helper'
 
-describe 'navigate' do
-  before do
-    @post = Post.create(title: "My Post", description: "My post desc")
+RSpec.describe Author, type: :model do
+  it "has a name" do
+    @author = Author.new
+    @author.name = "Joe Burgess"
+    expect(@author.name).to eq("Joe Burgess")
   end
 
-  it 'shows the title on the show page in a h1 tag' do
-    visit "/posts/#{@post.id}"
-    expect(page).to have_css("h1", text: "My Post")
+  it "has a genre" do
+    @author = Author.new
+    @author.genre = "Fiction"
+    expect(@author.genre).to eq("Fiction")
   end
 
-  it 'to post pages' do
-    visit "/posts/#{@post.id}"
-    expect(page.status_code).to eq(200)
-  end
-
-  it 'shows the description on the show page in a p tag' do
-    visit "/posts/#{@post.id}"
-    expect(page).to have_css("p", text: "My post desc")
-  end
-end
-
-describe 'form' do
-
-  it 'shows an update form that submits content and redirects and prints out params' do
-    @post = Post.create(title: "My Post", description: "My post desc")
-
-    visit edit_post_path(@post)
-
-    fill_in 'post[title]', with: "My edit"
-    fill_in 'post[description]', with: "My post description"
-
-    click_on "Update Post"
-
-    expect(page).to have_content("My Edit")
-  end
-end
-
-describe "post" do
-  it "has a post_status field" do
-    @post = Post.create(title: "My Post", description: "My post desc", post_status: true)
-    expect(@post.post_status).to eq(true)
+  it "has a bio" do
+    @author = Author.new
+    @author.bio = "I'm a Teacher!"
+    expect(@author.bio).to eq("I'm a Teacher!")
   end
 end
